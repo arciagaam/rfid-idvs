aa2cb87ac00fb48b23397f83334ef67e55817f14
Miguel <miguelarciagaa@gmail.com>
2023-12-12 16:50:16 +0800

    admin side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom"
 
 // Layouts
 import { RootLayout } from "./layout/RootLayout"
+import { AdminLayout } from "./layout/AdminLayout"
 
 // Pages
 import { Home } from "./pages/home"
 import { Login } from "./pages/login"
 import { ProtectedLayout } from "./layout/ProtectedLayout"
+import { Dashboard } from "./pages/admin/dashboard"
 
 function App() {
     return (
@@ -14,7 +16,16 @@ function App() {
             <Route element={<RootLayout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+
                 <Route element={<ProtectedLayout />}>
+
+                    <Route path="/admin" element={<AdminLayout />}>
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="user-management" element={<Dashboard />} />
+                        <Route path="student-management" element={<Dashboard />} />
+                        <Route path="school-year-management" element={<Dashboard />} />
+                    </Route>
+
                     <Route path="/test" element={<TestElement />}>
                     </Route>
                 </Route>
