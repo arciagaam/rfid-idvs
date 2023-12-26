b3898b364061425d4eabaddeef6e181c6a85fc8b
polaski0 <meynard.trinidad44@gmail.com>
2023-12-26 11:18:43 +0800

    Changes in user sideof student id validation index

--- client/src/pages/admin/student_id_validation/index.tsx
+++ client/src/pages/admin/student_id_validation/index.tsx
@@ -67,7 +67,6 @@ const StudentIDValidation = () => {
                     </TabsContent>
                 </Tabs>
             </div>
-
         </>
     )
 }
--- client/src/pages/user/student_id_validation/index.tsx
+++ client/src/pages/user/student_id_validation/index.tsx
@@ -1,12 +1,4 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
-import { StudentID } from './student_id';
-import { ValidationHistory } from './validation_history';
-
-const API_URL = import.meta.env.VITE_API_URL;
+import { StudentIDValidation as GlobalStudentIDValidation } from '@/pages/admin/student_id_validation';
 
 export type TCourse = {
     id: number;
@@ -15,61 +7,7 @@ export type TCourse = {
 };
 
 const StudentIDValidation = () => {
-    const { slug } = useParams();
-    const [courses, setCourses] = useState<TCourse[]>([]);
-
-    useEffect(() => {
-        const fetchDepartmentCourses = async () => {
-            try {
-                const req = await fetch(`${API_URL}/departments/${slug}`, {
-                    credentials: 'include',
-                });
-
-                if (!req.ok) {
-                    throw await req.json();
-                }
-
-                const res = await req.json();
-                const responseData = res.data;
-                const _courses = responseData.courses.map((course: { id: number, name: string }) => {
-                    return {
-                        ...course,
-                        value: course.name
-                    }
-                });
-
-                setCourses(_courses);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        if (slug !== undefined) {
-            fetchDepartmentCourses();
-        }
-    }, [slug]);
-
-    return (
-        <>
-            <div className="flex flex-col gap-6 w-full justify-between">
-                <h2 className='text-lg font-bold'>ID Validation - {slug !== undefined ? slug.toUpperCase() : "Invalid Department"}</h2>
-
-                <Tabs defaultValue="student_id">
-                    <TabsList>
-                        <TabsTrigger value="student_id">Student ID</TabsTrigger>
-                        <TabsTrigger value="validation_history">Validation History</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="student_id">
-                        <StudentID slug={slug} courses={courses} />
-                    </TabsContent>
-                    <TabsContent value="validation_history">
-                        <ValidationHistory />
-                    </TabsContent>
-                </Tabs>
-            </div>
-
-        </>
-    )
+    return <GlobalStudentIDValidation />
 }
 
 export { StudentIDValidation }
