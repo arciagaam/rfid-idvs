5df982584abbcdaaf816797c2949896932cb741e
Miguel <miguelarciagaa@gmail.com>
2024-01-04 03:14:44 +0800

    logo login

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -5,7 +5,9 @@ const Login = () => {
         <div className="w-full h-screen flex justify-center items-center">
             <div className="flex flex-col items-center gap-8">
                 {/* Insert logo here */}
-                <div className="w-32 aspect-square bg-neutral-300 rounded-full"></div>
+                <div className="w-32 aspect-square bg-white rounded-full overflow-clip">
+                    <img className="object-cover" src="/images/logo.png" alt="" />
+                </div>
 
                 <div className="flex flex-col gap-2 text-center">
                     <strong className="font-bold text-3xl">Login to your account</strong>
