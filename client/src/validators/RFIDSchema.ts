8dacb781a861e096a705773894926eb1f6ef55e6
polaski0 <meynard.trinidad44@gmail.com>
2024-01-26 18:54:36 +0800

    Minor fix in rfid link

--- client/src/validators/RFIDSchema.ts
+++ client/src/validators/RFIDSchema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const RFIDSchema = z.object({
     id: z.coerce.number(),
-    rfid_number: z.string().optional().nullable()
+    rfid_number: z.string().min(1)
 });
